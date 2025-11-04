450ec5d74b23fd9f98999f3a942d0d041b978536
kosa <kosa@DESKTOP-BNOMSU0>
2025-11-05 08:43:31 +0900

    Day 2 Commit

--- Day2/src/quiz/Quiz_ex03.java
+++ Day2/src/quiz/Quiz_ex03.java
@@ -6,8 +6,20 @@ public class Quiz_ex03 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("년도를 입력해주세요 :");
-		int year = sc.nextInt();
-		
+
+        System.out.print("연도를 입력하세요: ");
+        int year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + "년은 윤년입니다.");
+            } else {
+                System.out.println(year + "년은 평년입니다.");
+            }
+        } else {
+            System.out.println(year + "년은 평년입니다.");
+        }
+
+        sc.close();
 	}
 }
